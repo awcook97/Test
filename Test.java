a2efd1bc6f1fb7bda7a04b3fa2d853787134658d
Axel <awcook97@gmail.com>
2020-04-27 11:51:27 -0500

    Test Feature 2

--- Test.java
+++ Test.java
@@ -1,11 +1,15 @@
 public class Test {
     private bool validTest;
-    
+    private int testFeat;
     public Test() {
         validTest = true;
+        testFeat = 1;
     }
 
-    public Test(bool validTest) {
+    public Test(bool validTest, int testFeat) {
         this.validTest = validTest;
+        this.testFeat = testFeat;
     }
+
+
 }
